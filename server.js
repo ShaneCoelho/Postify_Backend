acdfffe6bccd46e7574c7578298996b83254e944
ShaneCoelho <shanecoelho1@gmail.com>
2024-01-03 12:40:44 +0530

    install the dependencies and created the server and database file

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+require("dotenv").config();
+const ConnectionDB =require("./database");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const User = require('./models/user');
+const helmet=require('helmet')
+const Posts = require('./models/posts');
+ConnectionDB();
+
+const app = express()
+
+
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+//Available Routes
+app.use("/api/auth",require("./routes/auth"))
+
+
+app.listen(port, () => {
+    console.log(` backend listening at http://localhost:${port}`)
+  })
